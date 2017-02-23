f1e903f9e19183ef713bd7de4c0f24bca36aba50
张磊的电脑 <zhangleidediannao@zhangleidediannaodeMacBook-Pro.local>
2017-02-23 21:26:45 +0800

    implement is_member_of?(group)

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,7 +2,8 @@ class Group < ApplicationRecord
   belongs_to :user
   has_many :posts
   validates :title, presence: true
+  has_many :group_relationships
 
-  has_many :groups_relationships
   has_many :menbers, through: :group_relationships, source: :user
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-         has_many :groups_relationships
+         has_many :group_relationships
          has_many :participated_groups, :through => :group_relationships, :source => :group
+
+         def is_member_of?(group)
+           participated_groups.include?(group)
+end
 end
