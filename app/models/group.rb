d02b88db5ad08d67c976759063fee68d127e5482
张磊的电脑 <zhangleidediannao@zhangleidediannaodeMacBook-Pro.local>
2017-02-23 13:08:09 +0800

    implement group relation

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,7 @@ class Group < ApplicationRecord
   belongs_to :user
   has_many :posts
   validates :title, presence: true
+
+  has_many :groups_relationships
+  has_many :menbers, through: :group_relationships, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-         has_many :groups
-         has_many :posts
+         has_many :groups_relationships
+         has_many :participated_groups, :through => :group_relationships, :source => :group
 end
